e390bd74281ccd988e3090dcfdc54da730b88c6f
Wagner <kenji.yatabe@gmail.com>
2018-03-02 17:15:07 -0300

    Ajuste dos vídeos.

--- src/pages/ensino/ensino.ts
+++ src/pages/ensino/ensino.ts
@@ -16,16 +16,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class EnsinoPage {
   videos = [
     {
+      id: 1,
       url: "./assets/video/video_1.mp4",
       titulo: "Vídeo Número 1",
       descricao: "Introdução do aplicativo iFé.",
     },
     {
+      id: 2,
       url: "./assets/video/video_2.mp4",
       titulo: "Vídeo Número 2",
       descricao: "Como deve ser realizada a venda de consórcios.",
     },
     {
+      id: 3,
       url: "./assets/video/video_3.mp4",
       titulo: "Vídeo Número 3",
       descricao: "Como você receberá sua comissão?",
